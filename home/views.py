c3ee48221a225e0f288b7bc46a9737c9a895f7f1
EmreOzturk <emreozturk5761@gmail.com>
2020-04-03 18:36:01 +0300

    hakkimizda-referanslar-iletisim sayfalari eklendi

--- home/views.py
+++ home/views.py
@@ -9,5 +9,23 @@ def index(request):
     settings = Setting.objects.get(pk=1)
     context = {'settings': settings}
     return render(request, 'index.html', context)
-
     # return HttpResponse(" Deneme Sayfası %s." % text)
+
+
+def hakkimizda(request):
+    settings = Setting.objects.get(pk=1)
+    context = {'settings': settings}
+    return render(request, 'hakkimizda.html', context)
+
+
+def referanslar(request):
+    settings = Setting.objects.get(pk=1)
+    context = {'settings': settings}
+    return render(request, 'referanslar.html', context)
+
+
+def iletisim(request):
+    settings = Setting.objects.get(pk=1)
+    context = {'settings': settings}
+    return render(request, 'iletisim.html', context)
+
